dc422d1a0c166bfd99d36ad5f6c092a8529f866a
Alko <alkoknijff@gmail.com>
2021-01-12 16:42:29 +0100

    Actually using arsens functions

--- model/processing_unit.py
+++ model/processing_unit.py
@@ -1,11 +1,12 @@
 import torch.nn as nn
-from complexLayers import ComplexBatchNorm2d, ComplexConv2d, ComplexLinear
-from complexFunctions import complex_relu, complex_max_pool2d
+from complexLayers import ComplexConv2d, ComplexLinear
+from complexModules import *
 
 class ProcessingUnit(nn.Module):
     def __init__(self):
         super().__init__()
-        self.bn  = ComplexBatchNorm2d(20)
+        self.bn = InvariantBatchNorm()
+        self.relu = InvariantReLU()
         self.conv2 = ComplexConv2d(20, 50, 5, 1)
         self.fc1 = ComplexLinear(4*4*50, 500)
         self.fc2 = ComplexLinear(500, 200)
@@ -13,12 +14,12 @@ class ProcessingUnit(nn.Module):
     def forward(self, xr, xi):
         xr,xi = self.bn(xr,xi)
         xr,xi = self.conv2(xr,xi)
-        xr,xi = complex_relu(xr,xi)
-        xr,xi = complex_max_pool2d(xr,xi, 2, 2)
+        xr,xi = self.relu(xr,xi)
+        xr,xi = invmaxpool2d(xr,xi, 2, 2)
         
         xr = xr.view(-1, 4*4*50)
         xi = xi.view(-1, 4*4*50)
         xr,xi = self.fc1(xr,xi)
-        xr,xi = complex_relu(xr,xi)
+        xr,xi = self.relu(xr,xi)
         xr,xi = self.fc2(xr,xi)
         return xr, xi
