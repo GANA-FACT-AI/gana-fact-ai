958631184e3f989c36558c647a3f290b50c8eb70
Alko <alkoknijff@gmail.com>
2021-01-12 14:44:43 +0100

    Fixed rotating back problem

--- model/decoder.py
+++ model/decoder.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc3 = nn.Linear(128 * 200, 10)
+        self.fc3 = nn.Linear(200, 10)
 
     def forward(self, x):
         x = self.fc3(torch.flatten(x, start_dim=1))
