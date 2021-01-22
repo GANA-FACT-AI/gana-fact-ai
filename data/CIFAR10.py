998060ebd6cdbdd9e983264f75c701a1257fbb74
Georg Lange <georg.lange@student.hpi.uni-potsdam.de>
2021-01-22 06:00:37 +0100

    implemented adversary attack (the second one) added unet implementation added tensorboard logging of images added script for training adversary

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -3,17 +3,17 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def load_data(batch_size, num_workers):
+def load_data(batch_size, num_workers, path='./data'):
     transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root=path, train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=num_workers, drop_last=True, pin_memory=True)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.CIFAR10(root=path, train=False,
                                         download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=num_workers, drop_last=True, pin_memory=True)
