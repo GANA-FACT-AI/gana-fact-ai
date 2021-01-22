6a74d7d63fea3026f26dedefe941b4d4b5d95562
Alko <alkoknijff@gmail.com>
2021-01-22 15:41:42 +0100

    Forgot to push adversary changes

--- adversary/Adversary.py
+++ adversary/Adversary.py
@@ -14,6 +14,7 @@ class Adversary(pl.LightningModule):
         self.privacy_model = privacy_model
         self.loss = nn.MSELoss()  # TODO: is this the right loss function? In the paper, they used a different one, but they did segmentation and we image generation
         self.random_batch = None
+        self.img_counter = 0
 
     def forward(self, xr, xi):
         concats = torch.cat((xr, xi), 1)
@@ -37,8 +38,8 @@ class Adversary(pl.LightningModule):
 
         loss = self.loss(output, x)
         self.log("image_reconstruction_loss", loss)
-        if batch_idx % 30 == 0:  # TODO: move this code entirely into validation_step
-            self.validation_step_c(batch, batch_idx, I_prime)
+        #if batch_idx % 30 == 0:  # TODO: move this code entirely into validation_step
+            #self.validation_step_c(batch, batch_idx, I_prime)
 
         return loss
 
@@ -52,3 +53,14 @@ class Adversary(pl.LightningModule):
         img_grid_gen = torchvision.utils.make_grid(imgs/2 + 0.5)
         self.logger.experiment.add_image('original images', img_grid_orig, batch_idx)
         self.logger.experiment.add_image('reconstructed images', img_grid_gen, batch_idx)
+
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+        I_prime = x if self.random_batch is None else self.random_batch
+        self.random_batch = x
+        imgs = self.gen_imgs(x, I_prime)
+        img_grid_orig = torchvision.utils.make_grid(x/2 + 0.5)
+        img_grid_gen = torchvision.utils.make_grid(imgs/2 + 0.5)
+        self.logger.experiment.add_image('original images {}'.format(self.img_counter), img_grid_orig, batch_idx)
+        self.logger.experiment.add_image('reconstructed images {}'.format(self.img_counter), img_grid_gen, batch_idx)
+        self.img_counter += 1
