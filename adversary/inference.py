0a692bc8fbd0c8cbf7690e73b43c60104e4dcdf3
noudcorten <NRMCorten@gmail.com>
2021-01-27 12:51:57 +0100

    Added choice for resnet-beta variant
    e.g. --model 'resnet56b' now creates the correct variant of the resnet56 architecture

--- adversary/inference.py
+++ adversary/inference.py
@@ -6,40 +6,47 @@ from torch.optim import Adam
 
 # from adversary.unet import UNet
 from resnet import ResNet, BasicBlock
+from alexnet import AlexNet
 import torch.nn.functional as F
 
 class Inference(pl.LightningModule):
-    def __init__(self, inversion=None):
+    def __init__(self, dataset, inversion=None):
         super(Inference, self).__init__()
         self.random_batch = None
-        self.resnet56 = ResNet(BasicBlock, [9, 9, 9], num_classes=100)
         self.inversion = inversion
         self.accuracy = pl.metrics.Accuracy()
 
+        if dataset == 'cifar100':
+            self.model = ResNet(BasicBlock, [9, 9, 9], num_classes=100)
+        else:   # dataset == 'celeba'
+            self.model = AlexNet(num_classes=100)
+
     def forward(self, batch):
-        x, _ = batch
+        x, target = batch
         I_prime = x if self.random_batch is None else self.random_batch
         self.random_batch = x
 
         rec_img = self.inversion.gen_imgs(x, I_prime)
+        out = self.model(rec_img)
+        pred = F.log_softmax(out, dim=1)
+        rec_error = 1.0 - self.accuracy(pred, target)
 
-
-        return self.resnet56(rec_img)
+        return rec_error
 
     def training_step(self, batch, batch_idx):
         x, target = batch
-        out = self.resnet56(x)              
+        out = self.model(x)   
         pred = F.log_softmax(out, dim=1)
 
         loss = F.nll_loss(pred, target)
-        last_accuracy = self.accuracy(pred, target)
+        accuracy = self.accuracy(pred, target)
 
         if batch_idx % 50 == 0:
-            print("Accuracy: {}".format(last_accuracy))
+            print(" Accuracy: {}".format(accuracy))
         return loss
 
     def configure_optimizers(self):
-        return Adam(self.resnet56.parameters(), lr=1e-4)  # given in the UNet-paper
+        return Adam(self.model.parameters(), lr=1e-2)  # given in the UNet-paper
 
     # def validation_step_c(self, batch, batch_idx, I_prime):
     #     x, _ = batch
